e03df65f8daa572efd53b509c29dfe0bb6999c84
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-04-15 14:32:43 +0530

    Update AnalFreqSans.py

--- AnalFreqSans.py
+++ AnalFreqSans.py
@@ -8,8 +8,8 @@ import requests
 import codecs
 
 def count_words_in_text(text, words):
-    words_text = re.findall(r'\w+', text.lower())
-    return [words_text.count(word.lower()) for word in words]
+    words_text = re.findall(r'\w+', text, flags=re.UNICODE)  # Use Unicode flag for Devanagari text
+    return [words_text.count(word) for word in words]
 
 st.title('Multi-Entity Frequency Analyzer - Adi Parva')
 
@@ -22,7 +22,7 @@ file_paths = ['https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/BR-Comple
               'https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/MBTN.txt']
 
 # Define the file names
-file_names = ['BD', 'KMG', 'MND']
+file_names = ['BD', 'KMG', 'MND', 'MBTN']
 
 if st.button('Analyze'):
     if words_to_search:
@@ -30,8 +30,7 @@ if st.button('Analyze'):
         data = []
         for file_path, file_name in zip(file_paths, file_names):
             response = requests.get(file_path)
-            text = response.text
-            text = codecs.decode(text, 'unicode_escape')  # Decode Unicode escape sequences
+            text = response.content.decode('utf-8')  # Decode content properly
             counts = count_words_in_text(text, words_to_search)
             for word, count in zip(words_to_search, counts):
                 data.append([file_name, word, count])
@@ -42,6 +41,8 @@ if st.button('Analyze'):
         # Create a bar plot
         sns.barplot(x='Word', y='Frequency', hue='File', data=df)
         plt.title('Frequency of each word in each file')
+        plt.xticks(rotation=90)  # Rotate x-axis labels for better readability
         st.pyplot(plt)
     else:
         st.write("Please input words (comma separated).")
+
