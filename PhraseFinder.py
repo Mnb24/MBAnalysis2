3ef2a5e1ca9417cb57e44726d85ebac53cc2a173
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-04-19 12:27:46 +0530

    Update PhraseFinder.py

--- PhraseFinder.py
+++ PhraseFinder.py
@@ -18,8 +18,7 @@ def find_matches(text, input_text, delimiter):
     for line in lines:
         if re.search(r'\b' + re.escape(input_text) + r'\b', line):
             line = re.sub(r'\b' + re.escape(input_text) + r'\b', f'<span style="color:red">{input_text}</span>', line)
-            matched_lines.append(delimiter + line)
-            break  # Stop after finding the first match
+            matched_lines.append(line.strip())
     return matched_lines
 
 def main():
@@ -29,9 +28,6 @@ def main():
     kk_text = fetch_text("https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/KK-Complete.txt")
 
     # Display side pane with text boxes
-    st.sidebar.subheader("SV Text")
-    sv_text_input = st.sidebar.text_area("SV Text", sv_text, height=400)
-
     st.sidebar.subheader("Input Text")
     input_text = st.sidebar.text_area("Input Text", height=200)
 
@@ -39,9 +35,9 @@ def main():
 
     # Display matches in main portion
     st.title("Matches in BORI and KK Editions")
-    if find_matches_button:
-        if br_text and input_text:
-            matched_lines = find_matches(br_text, input_text, ' BR')
+    if find_matches_button and input_text:
+        if br_text:
+            matched_lines = find_matches(br_text, input_text, 'BR')
             if matched_lines:
                 st.write("Matches found in BORI edition:")
                 for line in matched_lines:
@@ -49,14 +45,16 @@ def main():
             else:
                 st.write("No matches found in BORI edition.")
         
-        if kk_text and input_text:
-            matched_lines = find_matches(kk_text, input_text, ' KK')
+        if kk_text:
+            matched_lines = find_matches(kk_text, input_text, 'KK')
             if matched_lines:
                 st.write("Matches found in KK edition:")
                 for line in matched_lines:
                     st.markdown(line, unsafe_allow_html=True)
             else:
                 st.write("No matches found in KK edition.")
+    else:
+        st.write("Please enter input text.")
 
 if __name__ == "__main__":
     main()
